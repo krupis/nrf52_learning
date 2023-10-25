e42edad5737bae65b6cbddf50c195a38ba509cba
Lukas Petrikas <petrikaslukas@gmail.com>
2023-10-25 08:30:15 +0300

    removed build and build_1

--- build/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52dk_nrf52832" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "c:/users/petrikas.lu/desktop/work/nrf/nrf52_learning;C:/ncs/v2.4.0/nrf;C:/ncs/v2.4.0/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "C:/Users/petrikas.lu/Desktop/WORK/NRF/nrf52_learning/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "C:/Users/petrikas.lu/Desktop/WORK/NRF/nrf52_learning/boards/nrf52dk_nrf52832.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:\ncs\toolchains\31f4403e35\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")

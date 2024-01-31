5866ba01e5359ae22ef16dd5f0460bbe567449d3
Lukas <petrikaslukas@gmail.com>
2024-01-31 14:36:35 +0200

    Interrupt uart working with uart0

--- src/main.c
+++ src/main.c
@@ -9,10 +9,8 @@
 #include "zephyr/drivers/uart.h"
 
 static const struct device *dev_uart0;
-static const struct device *dev_uart1;
 
 static void uart0_irq_handler(const struct device *dev, void *context);
-//static void uart1_irq_handler(const struct device *dev, void *context);
 
 
 #define RX_BUF_SIZE 64
@@ -55,56 +53,6 @@ static void uart0_irq_handler(const struct device *dev, void *context)
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-// void app_uart1_init()
-// {
-// 	dev_uart1 = DEVICE_DT_GET(DT_NODELABEL(uart1));
-// 	if (!device_is_ready(dev_uart1))
-// 	{
-// 		return;
-// 	}
-// 	uart_irq_callback_set(dev_uart1, uart1_irq_handler);
-// 	uart_irq_rx_enable(dev_uart1);
-// }
-
-
-
-// static void uart1_irq_handler(const struct device *dev, void *context)
-// {
-// 	uint8_t char_received;
-// 	static uint8_t command_line[RX_BUF_SIZE];
-// 	static int char_counter = 0;
-
-// 	if (uart_irq_rx_ready(dev))
-// 	{
-// 		int len = uart_fifo_read(dev, &char_received, 1);
-// 		if (len)
-// 		{
-// 			command_line[char_counter] = char_received;
-// 			char_counter++;
-// 			if (char_received == '\n' || char_received == '\r')
-// 			{
-// 				command_line[char_counter] = '\0'; // put null at the end of the string to indicate end of message or end of string
-// 				char_counter = 0;				   // reset the char counter
-// 				printk("command_line = %s \n", command_line);
-// 				memset(&command_line, 0, sizeof(command_line));
-// 			}
-// 		}
-// 	}
-// }
-
-
-
-
 
 
 

532b92488b5aed36aa2588e70c5eb092ac106103
Lukas <petrikaslukas@gmail.com>
2024-01-31 14:17:01 +0200

    Project builts with ncs 2.5.1

--- src/main.c
+++ src/main.c
@@ -8,23 +8,31 @@
 #include "stdio.h"
 #include "zephyr/drivers/uart.h"
 
-static const struct device *dev_uart;
-static void uart_irq_handler(const struct device *dev, void *context);
+static const struct device *dev_uart0;
+static const struct device *dev_uart1;
+
+static void uart0_irq_handler(const struct device *dev, void *context);
+//static void uart1_irq_handler(const struct device *dev, void *context);
+
 
 #define RX_BUF_SIZE 64
 
-void app_uart_init()
+
+
+void app_uart0_init()
 {
-	dev_uart = DEVICE_DT_GET(DT_NODELABEL(uart0));
-	if (!device_is_ready(dev_uart))
+	dev_uart0 = DEVICE_DT_GET(DT_NODELABEL(uart0));
+	if (!device_is_ready(dev_uart0))
 	{
-		return 0;
+		return;
 	}
-	uart_irq_callback_set(dev_uart, uart_irq_handler);
-	uart_irq_rx_enable(dev_uart);
+	uart_irq_callback_set(dev_uart0, uart0_irq_handler);
+	uart_irq_rx_enable(dev_uart0);
 }
 
-static void uart_irq_handler(const struct device *dev, void *context)
+
+
+static void uart0_irq_handler(const struct device *dev, void *context)
 {
 	uint8_t char_received;
 	static uint8_t command_line[RX_BUF_SIZE];
@@ -48,7 +56,60 @@ static void uart_irq_handler(const struct device *dev, void *context)
 	}
 }
 
+
+
+
+
+
+
+
+
+
+// void app_uart1_init()
+// {
+// 	dev_uart1 = DEVICE_DT_GET(DT_NODELABEL(uart1));
+// 	if (!device_is_ready(dev_uart1))
+// 	{
+// 		return;
+// 	}
+// 	uart_irq_callback_set(dev_uart1, uart1_irq_handler);
+// 	uart_irq_rx_enable(dev_uart1);
+// }
+
+
+
+// static void uart1_irq_handler(const struct device *dev, void *context)
+// {
+// 	uint8_t char_received;
+// 	static uint8_t command_line[RX_BUF_SIZE];
+// 	static int char_counter = 0;
+
+// 	if (uart_irq_rx_ready(dev))
+// 	{
+// 		int len = uart_fifo_read(dev, &char_received, 1);
+// 		if (len)
+// 		{
+// 			command_line[char_counter] = char_received;
+// 			char_counter++;
+// 			if (char_received == '\n' || char_received == '\r')
+// 			{
+// 				command_line[char_counter] = '\0'; // put null at the end of the string to indicate end of message or end of string
+// 				char_counter = 0;				   // reset the char counter
+// 				printk("command_line = %s \n", command_line);
+// 				memset(&command_line, 0, sizeof(command_line));
+// 			}
+// 		}
+// 	}
+// }
+
+
+
+
+
+
+
+
 int main(void)
 {
-	app_uart_init();
+	app_uart0_init();
 }

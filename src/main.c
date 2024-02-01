10fe87f97f103dae833e76fa16f603a109068b87
Lukas <petrikaslukas@gmail.com>
2024-02-01 10:31:51 +0200

    Logging not working

--- src/main.c
+++ src/main.c
@@ -7,21 +7,26 @@
 #include <zephyr/kernel.h>
 #include "stdio.h"
 #include "zephyr/drivers/uart.h"
+#include "zephyr/logging/log.h"
 
 static const struct device *dev_uart0;
+static const struct device *dev_uart1;
+
 
 static void uart0_irq_handler(const struct device *dev, void *context);
+static void uart1_irq_handler(const struct device *dev, void *context);
 
 
 #define RX_BUF_SIZE 64
 
 
-
+//Initialize UART0
 void app_uart0_init()
 {
 	dev_uart0 = DEVICE_DT_GET(DT_NODELABEL(uart0));
 	if (!device_is_ready(dev_uart0))
 	{
+		printk("UART0 is not ready \n");
 		return;
 	}
 	uart_irq_callback_set(dev_uart0, uart0_irq_handler);
@@ -47,7 +52,7 @@ static void uart0_irq_handler(const struct device *dev, void *context)
 			{
 				command_line[char_counter] = '\0'; // put null at the end of the string to indicate end of message or end of string
 				char_counter = 0;				   // reset the char counter
-				printk("command_line = %s \n", command_line);
+				printk("UART0 Received = %s \n", command_line);
 				memset(&command_line, 0, sizeof(command_line));
 			}
 		}
@@ -57,7 +62,122 @@ static void uart0_irq_handler(const struct device *dev, void *context)
 
 
 
+//Initialize UART1
+void app_uart1_init()
+{
+	dev_uart1 = DEVICE_DT_GET(DT_NODELABEL(uart1));
+	if (!device_is_ready(dev_uart1))
+	{
+		printk("UART1 is not ready \n");
+		return;
+	}
+	uart_irq_callback_set(dev_uart1, uart1_irq_handler);
+	uart_irq_rx_enable(dev_uart1);
+}
+
+
+
+// static void uart1_irq_handler(const struct device *dev, void *context)
+// {
+// 	uint8_t char_received;
+// 	static uint8_t command_line[RX_BUF_SIZE];
+// 	static int char_counter = 0;
+
+// 	if (uart_irq_rx_ready(dev))
+// 	{
+// 		int len = uart_fifo_read(dev, &char_received, 1);
+// 		if (len)
+// 		{
+// 			command_line[char_counter] = char_received;
+// 			char_counter++;
+// 			if (char_received == '\n' || char_received == '\r')
+// 			{
+// 				command_line[char_counter] = '\0'; // put null at the end of the string to indicate end of message or end of string
+// 				char_counter = 0;				   // reset the char counter
+// 				printk("UART1 Received = %s \n", command_line);
+// 				memset(&command_line, 0, sizeof(command_line));
+// 			}
+// 		}
+// 	}
+// }
+
+
+
+
+
+
+
+
+
+
+static K_SEM_DEFINE(tx_sem, 0, 1); // Semaphore to signal UART data transmitted
+
+static uint8_t* tx_data;
+static size_t tx_data_length;
+
+static void uart1_irq_handler(const struct device *dev, void *user_data)
+{
+    ARG_UNUSED(user_data);
+    static int tx_data_idx;
+    if (!uart_irq_update(dev))
+    {
+            LOG_ERR("Couldn't update IRQ\n");
+            return;
+    }
+    if (uart_irq_tx_ready(dev) && tx_data_idx < tx_data_length)
+    {
+        int tx_send = MIN(CONFIG_UART_1_NRF_TX_BUFFER_SIZE, tx_data_length - tx_data_idx);
+        int tx_sent = uart_fifo_fill(dev, (uint8_t *)&tx_data[tx_data_idx], tx_send);
+        if (tx_sent <= 0)
+        {
+             LOG_ERR("Error %d sending data over RS485 bus\n", tx_sent);
+             return;
+        }
+        tx_data_idx += tx_sent;
+        if (tx_data_idx == tx_data_length)
+        {
+            uart_irq_tx_disable(dev);
+            tx_data_idx = 0;
+            k_sem_give(&tx_sem);
+        }
+    }
+ }
+
+int uart_irq_tx(const struct device *dev, const uint8_t *buf, size_t len)
+
+{
+
+    tx_data = buf;
+    tx_data_length = len;
+    uart_irq_callback_set(dev, uart1_irq_handler);
+    uart_irq_tx_enable(dev);
+    k_sem_take(&tx_sem, K_MSEC(1000));
+    LOG_INF("UART data transmitted\n");
+    return len; 
+}
+
+
+
+
+
+
+
+
+
+
 int main(void)
 {
-	app_uart0_init();
+	//app_uart0_init();
+	app_uart1_init();
+	while(1){
+		printk("Sending message from UART1\n");
+		char* msg = "Hello from UART1\n";
+		//int err = uart_tx(dev_uart1, &msg, 17, 1000);
+		//int err = uart_fifo_fill(dev_uart1, (uint8_t*)msg, 17);
+		// if(err != 0){
+		// 	printk("Error sending message from UART1 = %i\n",err);
+		// }
+
+		k_msleep(1000);
+	}
 }

2935e97d30e492e8389f9ad57c565f07a4f693c2
Lukas <petrikaslukas@gmail.com>
2024-02-01 11:37:27 +0200

    Testing things out

--- src/main.c
+++ src/main.c
@@ -7,15 +7,26 @@
 #include <zephyr/kernel.h>
 #include "stdio.h"
 #include "zephyr/drivers/uart.h"
-#include "zephyr/logging/log.h"
+#define LOG_LEVEL 4
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(nrf52_learning);
 
 static const struct device *dev_uart0;
 static const struct device *dev_uart1;
 
 
 static void uart0_irq_handler(const struct device *dev, void *context);
-static void uart1_irq_handler(const struct device *dev, void *context);
 
+
+
+
+static void uart1_irq_receive_handler(const struct device *dev, void *context);
+static void uart1_irq_transmit_handler(const struct device *dev, void *context);
+
+static K_SEM_DEFINE(tx_sem, 0, 1); // Semaphore to signal UART data transmitted
+
+static uint8_t* tx_data;
+static size_t tx_data_length;
 
 #define RX_BUF_SIZE 64
 
@@ -71,51 +82,39 @@ void app_uart1_init()
 		printk("UART1 is not ready \n");
 		return;
 	}
-	uart_irq_callback_set(dev_uart1, uart1_irq_handler);
+	uart_irq_callback_set(dev_uart1, uart1_irq_receive_handler);
 	uart_irq_rx_enable(dev_uart1);
 }
 
 
 
-// static void uart1_irq_handler(const struct device *dev, void *context)
-// {
-// 	uint8_t char_received;
-// 	static uint8_t command_line[RX_BUF_SIZE];
-// 	static int char_counter = 0;
+static void uart1_irq_receive_handler(const struct device *dev, void *context)
+{
+	uint8_t char_received;
+	static uint8_t command_line[RX_BUF_SIZE];
+	static int char_counter = 0;
 
-// 	if (uart_irq_rx_ready(dev))
-// 	{
-// 		int len = uart_fifo_read(dev, &char_received, 1);
-// 		if (len)
-// 		{
-// 			command_line[char_counter] = char_received;
-// 			char_counter++;
-// 			if (char_received == '\n' || char_received == '\r')
-// 			{
-// 				command_line[char_counter] = '\0'; // put null at the end of the string to indicate end of message or end of string
-// 				char_counter = 0;				   // reset the char counter
-// 				printk("UART1 Received = %s \n", command_line);
-// 				memset(&command_line, 0, sizeof(command_line));
-// 			}
-// 		}
-// 	}
-// }
-
-
+	if (uart_irq_rx_ready(dev))
+	{
+		int len = uart_fifo_read(dev, &char_received, 1);
+		if (len)
+		{
+			command_line[char_counter] = char_received;
+			char_counter++;
+			if (char_received == '\n' || char_received == '\r')
+			{
+				command_line[char_counter] = '\0'; // put null at the end of the string to indicate end of message or end of string
+				char_counter = 0;				   // reset the char counter
+				printk("UART1 Received = %s \n", command_line);
+				memset(&command_line, 0, sizeof(command_line));
+			}
+		}
+	}
+}
 
 
 
-
-
-
-
-
-static K_SEM_DEFINE(tx_sem, 0, 1); // Semaphore to signal UART data transmitted
-
-static uint8_t* tx_data;
-static size_t tx_data_length;
-
-static void uart1_irq_handler(const struct device *dev, void *user_data)
+static void uart1_irq_transmit_handler(const struct device *dev, void *user_data)
 {
     ARG_UNUSED(user_data);
     static int tx_data_idx;
@@ -124,35 +123,72 @@ static void uart1_irq_handler(const struct device *dev, void *user_data)
             LOG_ERR("Couldn't update IRQ\n");
             return;
     }
-    if (uart_irq_tx_ready(dev) && tx_data_idx < tx_data_length)
+
+
+
+    if (uart_irq_tx_ready(dev))
     {
-        int tx_send = MIN(CONFIG_UART_1_NRF_TX_BUFFER_SIZE, tx_data_length - tx_data_idx);
-        int tx_sent = uart_fifo_fill(dev, (uint8_t *)&tx_data[tx_data_idx], tx_send);
+		int tx_sent = uart_fifo_fill(dev, (uint8_t *)tx_data, tx_data_length);
+
         if (tx_sent <= 0)
         {
-             LOG_ERR("Error %d sending data over RS485 bus\n", tx_sent);
+             LOG_ERR("Error %d sending data over UART1 bus\n", tx_sent);
              return;
         }
-        tx_data_idx += tx_sent;
-        if (tx_data_idx == tx_data_length)
-        {
-            uart_irq_tx_disable(dev);
-            tx_data_idx = 0;
-            k_sem_give(&tx_sem);
-        }
-    }
+
+		// tx_data_idx += tx_sent;
+        // if (tx_data_idx == tx_data_length)
+        // {
+		// 	LOG_INF("UART1 data transmitted\n");
+        //     uart_irq_tx_disable(dev);
+        //     tx_data_idx = 0;
+        //     k_sem_give(&tx_sem);
+        // }
+
+		while(uart_irq_tx_complete(dev) != 1){
+			LOG_INF("Wait for UART1 data transmition complete\n");
+		}
+		LOG_INF("UART1 data transmitted\n");
+		uart_irq_tx_disable(dev);
+		k_sem_give(&tx_sem);
+		
+	}
+
+
+    // if (uart_irq_tx_ready(dev) && tx_data_idx < tx_data_length)
+    // {
+	// 	LOG_INF("Transmitting data over UART1\n");
+	// 	LOG_INF("tx_data_length = %u \n",tx_data_length);
+	// 	LOG_INF("tx_data_idx = %u \n",tx_data_idx);
+    //     int tx_send = MIN(CONFIG_UART_1_NRF_TX_BUFFER_SIZE, tx_data_length - tx_data_idx);
+    //     int tx_sent = uart_fifo_fill(dev, (uint8_t *)&tx_data[tx_data_idx], tx_send);
+	    //     if (tx_sent <= 0)
+        // {
+        //      LOG_ERR("Error %d sending data over UART1 bus\n", tx_sent);
+        //      return;
+        // }
+
+    //     tx_data_idx += tx_sent;
+    //     if (tx_data_idx == tx_data_length)
+    //     {
+    //         uart_irq_tx_disable(dev);
+    //         tx_data_idx = 0;
+    //         k_sem_give(&tx_sem);
+    //     }
+	// }
+
+
+    
  }
 
 int uart_irq_tx(const struct device *dev, const uint8_t *buf, size_t len)
-
 {
-
     tx_data = buf;
     tx_data_length = len;
-    uart_irq_callback_set(dev, uart1_irq_handler);
+    uart_irq_callback_set(dev, uart1_irq_transmit_handler);
     uart_irq_tx_enable(dev);
     k_sem_take(&tx_sem, K_MSEC(1000));
-    LOG_INF("UART data transmitted\n");
+    //LOG_INF("UART data transmitted\n");
     return len; 
 }
 
@@ -167,17 +203,11 @@ int uart_irq_tx(const struct device *dev, const uint8_t *buf, size_t len)
 
 int main(void)
 {
-	//app_uart0_init();
+	app_uart0_init();
 	app_uart1_init();
 	while(1){
-		printk("Sending message from UART1\n");
-		char* msg = "Hello from UART1\n";
-		//int err = uart_tx(dev_uart1, &msg, 17, 1000);
-		//int err = uart_fifo_fill(dev_uart1, (uint8_t*)msg, 17);
-		// if(err != 0){
-		// 	printk("Error sending message from UART1 = %i\n",err);
-		// }
-
 		k_msleep(1000);
+		uart_irq_tx(dev_uart1, "Hello from UART1\n", 17);
 	}
+
 }

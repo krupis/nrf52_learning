978c1ecb3be29884c00579bf4acec8b31474a652
Lukas <petrikaslukas@gmail.com>
2024-02-13 13:52:52 +0200

    ✨ feat: logging through USB working
    Changelog: feature

--- components/my_gpio/my_gpio.h
+++
@@ -1,9 +0,0 @@
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
-
-#define TEST_123 5
-
-void configure_leds();
-void configure_buttons();
-void turn_on_red_led();
-void turn_on_blue_led();
--- src/main.c
+++ src/main.c
@@ -8,34 +8,27 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/poweroff.h>
 #include "stdio.h"
-#include "my_gpio.h"
-#include "uart0.h"
-#include "uart1.h"
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/usbd.h>
+#include <zephyr/drivers/uart.h>
 
+BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
+			 "Console device is not ACM CDC UART device");
 
 #define LOG_LEVEL 4
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(nrf52_learning);
 
-
-#define STACKSIZE 1024
-
-
-K_THREAD_DEFINE(uart0_parser, STACKSIZE, uart0_parser_thread, NULL, NULL, NULL, 7, 0, 0);
-K_THREAD_DEFINE(uart1_parser, STACKSIZE, uart1_parser_thread, NULL, NULL, NULL, 7, 0, 0);
-
-
 int main(void)
 {
-	app_uart0_init();
-	app_uart1_init();
+	if (usb_enable(NULL))
+	{
+		return 0;
+	}
 
-	//nrf_pwr_mgmt_run();
-	//sys_poweroff();
-	// while(1){	
-	// 	//uart_tx(dev_uart1, "Hello World\n", 12, 1000);
-	// 	//uart1_send_string("Hello World\n");
-	// 	k_sleep(K_MSEC(1000));
-	// }
-
+	while (1)
+	{
+		// LOG_INF("Hello");
+		k_sleep(K_MSEC(1000));
+	}
 }

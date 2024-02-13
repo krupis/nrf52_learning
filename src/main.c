de0ea69b2d6a9599c21e7da4e3b094662d9e9bc5
Lukas <petrikaslukas@gmail.com>
2024-02-13 15:12:45 +0200

    🔄 change: Minor update
    Changelog: changed

--- src/main.c
+++ src/main.c
@@ -12,30 +12,23 @@
 #include "uart0.h"
 #include "uart1.h"
 
-
 #define LOG_LEVEL 4
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(nrf52_learning);
 
-
 #define STACKSIZE 1024
-
 
 K_THREAD_DEFINE(uart0_parser, STACKSIZE, uart0_parser_thread, NULL, NULL, NULL, 7, 0, 0);
 K_THREAD_DEFINE(uart1_parser, STACKSIZE, uart1_parser_thread, NULL, NULL, NULL, 7, 0, 0);
-
 
 int main(void)
 {
 	app_uart0_init();
 	app_uart1_init();
 
-	//nrf_pwr_mgmt_run();
-	//sys_poweroff();
-	// while(1){	
-	// 	//uart_tx(dev_uart1, "Hello World\n", 12, 1000);
-	// 	//uart1_send_string("Hello World\n");
-	// 	k_sleep(K_MSEC(1000));
-	// }
-
+	while (1)
+	{
+		uart1_send_string("Hello World\n");
+		k_sleep(K_MSEC(1000));
+	}
 }
